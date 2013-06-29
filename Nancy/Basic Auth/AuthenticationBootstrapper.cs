1cae3a93c9c4173215eff727e92fc219a7baa82c
Jonathan Channon <jonathan.channon@gmail.com>
2013-06-29 19:38:23 +0100

    Nancy Basic Auth Sample

---
+++ Nancy/Basic Auth/AuthenticationBootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using Nancy.Authentication.Basic;
+using Nancy.Bootstrapper;
+using Nancy.Conventions;
+using Nancy.TinyIoc;
+using Nancy;
+
+namespace BasicAuth
+{
+    public class AuthenticationBootstrapper : DefaultNancyBootstrapper
+	{
+		protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+		{
+            base.ApplicationStartup(container, pipelines);
+
+            pipelines.EnableBasicAuthentication(new BasicAuthenticationConfiguration(
+                container.Resolve<IUserValidator>(),
+                "MyRealm"
+                ));
+		}
+
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/index.html", "/Content/index.html"));
+        }
+	}
+}
